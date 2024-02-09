5779e2cef58ac6cd40dea0c890a2ee1f8e7c0ea2
devtofunmi <olayiwolajesutofunmi@gmail.com>
2024-02-09 17:40:50 +0100

    adjust the padding

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,6 +1,6 @@
 export default function Topbar() {
   return (
-    <div className="md:p-2">
+    <div className="p-5 md:p-2">
      <h1 className="text-2xl text-green-500 font-semibold">Nameless</h1>
     </div>
   );
