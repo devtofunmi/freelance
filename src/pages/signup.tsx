fb461359cdbb7280fd0b155f91bd0e3a8bcc9f85
devtofunmi <olayiwolajesutofunmi@gmail.com>
2024-02-10 15:40:54 +0100

    add a form div

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -12,7 +12,8 @@ export default function SignUp() {
         <div className="flex  items-center justify-center">
      <div className="flex flex-col w-full px-5 md:px-0 md:w-[40%] justify-center items-center mt-20">
         <h1 className="text-3xl text-center font-semibold">Sign up</h1>
-        <div className="flex gap-5 w-full mt-10">
+        <form>
+          <div className="flex gap-5 w-full mt-10">
             <div className="w-full">
                 <h1 className="text-xl">First name</h1>
                 <input className="w-full rounded-lg py-2 px-1 mt-2 bg-transparent border-[1px] border-whute" type="text" />
@@ -30,9 +31,9 @@ export default function SignUp() {
             <h1 className="text-xl">Password</h1>
             <input className="w-full rounded-lg py-2 px-1 mt-2 bg-transparent border-[1px] border-whute" type="password" />
         </div>
-        <Link href={"/"}>
          <Button  text={"Create my account"} />
-        </Link>
+        </form>
+        
          <div className="flex gap-2 mt-5">
        <p>Already have an account?</p>
        <Link href="/login">
