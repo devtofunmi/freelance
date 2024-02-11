bfafbf4e9d1ab910a606c81bae92bace873c3e07
devtofunmi <olayiwolajesutofunmi@gmail.com>
2024-02-11 14:17:12 +0100

    add a form div

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -11,17 +11,17 @@ export default function Login() {
         <div className="flex  items-center justify-center">
      <div className="flex flex-col w-full px-5 md:px-0 md:w-[40%] justify-center items-center mt-20">
         <h1 className="text-3xl text-center font-semibold">Log in</h1>
-        <div className="w-full mt-5">
+        <form>
+          <div className="w-full mt-5">
             <h1 className="text-xl">Email</h1>
             <input className="w-full rounded-lg py-2 px-1 mt-2 bg-transparent border-[1px] border-whute" type="email" />
-        </div>
+        fo</div>
          <div className="w-full mt-5">
             <h1 className="text-xl">Password</h1>
             <input className="w-full rounded-lg py-2 px-1 mt-2 bg-transparent border-[1px] border-whute" type="password" />
         </div>
-        <Link href={"/"}>
          <Button text={"Log in"} />
-        </Link>
+        </form>     
          <div className="flex gap-2 mt-5">
        <p>Dont have an account?</p>
        <Link href="/signup">
