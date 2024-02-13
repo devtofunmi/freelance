4ab601bbff07d230c0f08a04256e18de7fb7a33c
devtofunmi <olayiwolajesutofunmi@gmail.com>
2024-02-13 23:18:29 +0100

    add handle submit function

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -5,70 +5,106 @@ import Button from "@/components/Button";
 import Link from "next/link";
 import { useState } from "react";
 
-
 export default function SignUp() {
-  const [firstName, setFirstName] =useState<string>('')
-  const [lastName, setLastName] =useState<string>('')
-  const [password, setPassword] =useState<string>('')
-  const [email, setEmail] =useState<string>('')
-  const [errorMessage, setErrorMessage] =useState<string>('errormessage')
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if(!firstName || ! lastName || email || password){
-      setErrorMessage("All fields are required")
+    if (!firstName || !lastName || !email || !password || !errorMessage) {
+      setErrorMessage("All fields are required");
       return;
     }
-  }
+
+    try {
+      const res = await fetch("api/signup", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          firstName,
+          lastName,
+          email,
+          password,
+        }),
+      });
+
+      if (res.ok) {
+        const form = e.currentTarget;
+        form.reset();
+      } else {
+        console.log("user registration failed.");
+      }
+    } catch (error) {
+      console.log("error during registration", error);
+    }
+  };
+
   return (
     <div>
-     <Topbar />
-        <div className="flex  items-center justify-center">
-     <div className="flex flex-col w-full px-5 md:px-0 md:w-[40%] justify-center items-center mt-20">
-      {
-        errorMessage && (
-      <div className="bg-red text-white py-1 px-3 rounded-md">{errorMessage}</div>
-        )
-      }
-        <h1 className="text-3xl text-center font-semibold">Sign up</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="flex gap-5 w-full mt-10">
-            <div className="w-full">
-                <h1 className="text-xl" onChange={(e) => {
-                  setFirstName(e.target.value)
-                }}>First name</h1>
-                <input className="w-full rounded-lg py-2 px-1 mt-2 bg-transparent border-[1px] border-whute" type="text" />
+      <Topbar />
+      <div className="flex items-center justify-center">
+        <div className="flex flex-col w-full px-5 md:px-0 md:w-[40%] justify-center items-center mt-20">
+          {errorMessage && (
+            <div className="bg-red-500 text-white w-fit px-3 py-1 text-lg rounded-lg">
+              {errorMessage}
             </div>
-             <div className="w-full">
-                <h1 className="text-xl" onChange={(e) => {
-                  setLastName(e.target.value)
-                }}>Last name</h1>
-                <input className="w-full rounded-lg py-2 px-1 mt-2 bg-transparent border-[1px] border-whute" type="name" />
+          )}
+
+          <h1 className="text-3xl text-center font-semibold mt-2">Sign up</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="flex gap-5 w-full mt-10">
+              <div className="w-full">
+                <h1 className="text-xl">First name</h1>
+                <input
+                  className="w-full rounded-lg py-2 px-1 mt-2 bg-transparent border-[1px] border-whute"
+                  type="text"
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </div>
+              <div className="w-full">
+                <h1 className="text-xl">Last name</h1>
+                <input
+                  className="w-full rounded-lg py-2 px-1 mt-2 bg-transparent border-[1px] border-whute"
+                  type="name"
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </div>
             </div>
+            <div className="w-full mt-5">
+              <h1 className="text-xl">Email</h1>
+              <input
+                className="w-full rounded-lg py-2 px-1 mt-2 bg-transparent border-[1px] border-whute"
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="w-full mt-5">
+              <h1 className="text-xl">Password</h1>
+              <input
+                className="w-full rounded-lg py-2 px-1 mt-2 bg-transparent border-[1px] border-whute"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="flex justify-center">
+              <Button text={"Create my account"} />
+            </div>
+          </form>
+
+          <div className="flex gap-2 mt-5">
+            <p>Already have an account?</p>
+            <Link href="/login">
+              <p className="underline hover:text-green-[500]">Login</p>
+            </Link>
+          </div>
         </div>
-        <div className="w-full mt-5">
-            <h1 className="text-xl" onChange={(e) => {
-                  setEmail(e.target.value)
-                }}>Email</h1>
-            <input className="w-full rounded-lg py-2 px-1 mt-2 bg-transparent border-[1px] border-whute" type="email" />
-        </div>
-         <div className="w-full mt-5">
-            <h1 className="text-xl"  onChange={(e) => {
-                  setPassword(e.target.value)
-                }}>Password</h1>
-            <input className="w-full rounded-lg py-2 px-1 mt-2 bg-transparent border-[1px] border-whute" type="password" />
-        </div>
-         <Button  text={"Create my account"} />
-        </form>
-        
-         <div className="flex gap-2 mt-5">
-       <p>Already have an account?</p>
-       <Link href="/login">
-       <p className="underline hover:text-green-[500]">Login</p></Link>
-       </div>
-     </div>
+      </div>
     </div>
-    </div> 
   );
 }
