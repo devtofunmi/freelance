2e35e558f1abb5cca67961d3339cf47c9eb436c9
devtofunmi <olayiwolajesutofunmi@gmail.com>
2024-02-13 23:23:58 +0100

    style the form div

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -11,16 +11,18 @@ export default function Login() {
         <div className="flex  items-center justify-center">
      <div className="flex flex-col w-full px-5 md:px-0 md:w-[40%] justify-center items-center mt-20">
         <h1 className="text-3xl text-center font-semibold">Log in</h1>
-        <form>
-          <div className="w-full mt-5">
+        <form className="w-full mt-5">
+          <div >
             <h1 className="text-xl">Email</h1>
             <input className="w-full rounded-lg py-2 px-1 mt-2 bg-transparent border-[1px] border-whute" type="email" />
-        fo</div>
+        </div>
          <div className="w-full mt-5">
             <h1 className="text-xl">Password</h1>
             <input className="w-full rounded-lg py-2 px-1 mt-2 bg-transparent border-[1px] border-whute" type="password" />
         </div>
+        <div className="flex justify-center">
          <Button text={"Log in"} />
+        </div>
         </form>     
          <div className="flex gap-2 mt-5">
        <p>Dont have an account?</p>
