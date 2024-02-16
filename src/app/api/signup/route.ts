0fd407137b4bc651d23852e9c016e77628efb451
devtofunmi <olayiwolajesutofunmi@gmail.com>
2024-02-16 12:16:16 +0100

    hash password using bycrypt

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,9 +1,14 @@
 import { NextResponse } from "next/server";
+import {connectMongoDB} from "@/../lib/mongodb";
+import User from "@/../models/user";
+import bcyrpt from "bcyrptjs";
 
 export async function POST(req: any) {
     try {
         const { firstName, lastName, email, password } = await req.json();
-        console.log(firstName, lastName, email, password);
+        const hashedPassword = await bcyrpt.hash(password, 10)
+        await connectMongoDB();
+        await User.create({firstName, lastName, email, password: hashedPassword});
         return NextResponse.json({ message: "User registered." }, { status: 201 });
     } catch (error) {
         console.error("Error during user registration:", error);
